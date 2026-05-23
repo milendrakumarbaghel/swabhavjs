06dc78dcb043d9e36033930d91a2839e13daa7f4
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2026-05-23 10:45:27 +0530

    completed js arithematic operator tuto

---
+++ 3. Arithmetic Operators/script.js
@@ -0,0 +1,56 @@
+/*
+arithmetic operators = operands (values, variables, etc..)
+                       operators(+,-,*,/)
+                       ex. 11 = x + 5;
+*/
+
+let students = 30;
+
+// students = students - 1;
+// students = students + 1;
+//students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+
+// students -= 1;
+// students += 1;
+//students += 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students += 1;
+// students -= 1;
+// students */ 2;
+// students /= 2;
+// students %= 3;
+
+//students++;
+//students--;
+
+// console.log(students);
+
+/*
+operators precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division & modul0
+    4. addition & subtraction
+*/
+
+
+
+// equation 1
+// let result = 1 + 2 * 3 + 4 ** 2;
+// // 1 + (2 * 3) + (4 ** 2); // 1 + 6 + 16 = 23
+
+// equation 2
+// let result = 12 % 5 + 8 / 2;
+// // (12 % 5) + (8 / 2); // 2 + 4 = 6
+
+// equation 3
+let result = 6 / 2 ** (2+5); // 6 / 2 ** 7;
+// // 6 / 128 = 0.046875
+
+console.log(result);
