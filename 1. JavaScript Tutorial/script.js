d3843898691589fec1e144af62107ee045d585c7
milendrakumarbaghel <baghelmilendrakumar@gmail.com>
2026-05-23 10:17:34 +0530

    JavaScript tutorial for beginners completed

---
+++ 1. JavaScript Tutorial/script.js
@@ -0,0 +1,15 @@
+// console.log(`Hello, World!`);
+// console.log(`I like pizza!`);
+// window.alert(`This is an alert!`);
+// window.alert(`i like Pizzza!`)
+
+document.getElementById(`myH1`).textContent = `Hello, World!`;
+document.getElementById(`myP`).textContent = `I like pizza!`;
+
+// This is a comment
+
+/*
+    This is
+    a
+    Comment
+*/
